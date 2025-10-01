ff078926797a5cc2d42375955d6ea2ef3fd913af
Josmeyly Yamil Duarte Amparo <josmeyly_duarte@ucne.edu.do>
2025-10-01 18:41:46 -0400

    Initial commits

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "josmeyly_duarte_P1_P2"
+include(":app")
+ 
